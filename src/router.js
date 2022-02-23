8958dc3f266a9be29b452b9344f461be505d903e
braaAwni <braaberoo95@gmail.com>
2022-02-23 12:07:21 +0200

    add xhr and files

--- src/router.js
+++ src/router.js
@@ -47,6 +47,20 @@ const router = (request, response) => {
           }
         })
       }
+      else if ( url === "/js/api.js"){
+        const filePath = path.join(__dirname,'..','public','js','api.js');
+        fs.readFile(filePath, (error, data) => {
+          if (error) {
+            response.writeHead(500)
+            response.end("server error")
+            return;
+          } 
+          else {
+            response.writeHead(200, { 'Content-Type': 'text/javasrcipt' })
+            response.end(data);
+          }
+        })
+      }
       else if ( url.includes("cars") && method==='POST'){
         let dataXhr = (url.split('/')[2])
             const filePath = path.join(__dirname,'cars.json');
@@ -65,13 +79,34 @@ const router = (request, response) => {
                   let aa=  result[i].Name
                   arr.push(aa)
                    }
-                   console.log(arr)
                    response.writeHead(200, { "Content-Type": "application/json" });
                    response.end(JSON.stringify(arr)); //data send to xhr
                    
                   }
               }); 
-      } 
+      } else if (url.includes("api") ){
+        let dataXhr = (url.split('/')[2])
+        // console.log(dataXhr)
+            https.get(`https://api.unsplash.com/photos/?client_id=wgMDUq6nkl18c9Eop2yezO0e0EB0VMp3rp172jAh_uA&query=${dataXhr}`, (res) => {
+                let data = '';
+                // A chunk of data has been received.
+                res.on('data', (chunk) => {
+                  data += chunk;
+                });
+                // The whole response has been received. Print out the result.
+                res.on('end', () => {
+                  let dataApi = JSON.parse(data)
+                  let unData= dataApi[0]
+                  // console.log(dataApi[0].urls.full);
+                  response.writeHead(200, { "Content-Type": "application/json" });
+                   response.end(JSON.stringify(unData)); //data send to xhr
+                });
+
+              }).on("error", (err) => {
+                console.log("Error: " + err.message);
+              });
+
+  }
      else{
         response.writeHead(404)
         response.end("not found")
